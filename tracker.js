6b0a2ddce484fa3dedd36730f41b12fca4fcf322
David <david@goyoo.com>
2017-01-06 17:05:17 +0800

    elems length

--- tracker.js
+++ tracker.js
@@ -111,13 +111,15 @@
 		log['req']= parseInt(entry.responseStart - entry.requestStart, 10)
 		log['res']= parseInt(entry.responseEnd - entry.responseStart, 10)
 		
-		log['get']= parseInt(log['req']+log['res'], 10)
+		// log['get']= parseInt(log['req']+log['res'], 10)
 		// log['tot']= parseInt(log['con']+log['wait']+log['req']+log['res'], 10)
 
 		if( 'domInteractive' in entry && 'fetchStart' in entry ){
 			log['load']= parseInt(entry.loadEventEnd - entry.navigationStart, 10)
 			log['dom']= parseInt(entry.domInteractive - entry.fetchStart, 10)
 		}
+
+		log['elms']= document.getElementsByTagName('*').length
 
 		// log['_ = entry
 
